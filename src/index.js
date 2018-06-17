8734eb3e7918e97fb86fa3e26099fe6ea8016600
Pakapon Kaewmark <sleep191@gmail.com>
2018-06-18 02:29:41 +0700

    :recycle: Move firebase config to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import firebase from 'firebase/app'
+
+// Initialize Firebase
+const config = {
+  apiKey: 'AIzaSyC1PQGh5JGgJtpmt1-dZKY7XPQgR12Xb6w',
+  authDomain: 'reactbkk-connect.firebaseapp.com',
+  databaseURL: 'https://reactbkk-connect.firebaseio.com',
+  projectId: 'reactbkk-connect',
+  storageBucket: 'reactbkk-connect.appspot.com',
+  messagingSenderId: '946641983889'
+}
+firebase.initializeApp(config)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
