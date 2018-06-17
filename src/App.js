66262eab85f3e48e6efecef479fcc12cb9299df8
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-06-18 00:03:34 +0700

    :sparkles: Use react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 import styled, { keyframes } from 'styled-components'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import logo from './logo.svg'
+import Redirect from 'react-router-dom/Redirect'
 
 const AppStyle = styled.div`
   text-align: center;
+`
+
+const Nav = styled.nav`
+  > ul {
+    display: flex;
+    list-style: none;
+
+    > li {
+      &:not(:first-child) {
+        margin-left: 1rem;
+      }
+
+      &:hover {
+        color: #666;
+      }
+    }
+  }
 `
 
 const Header = styled.header`
@@ -31,22 +50,36 @@ const Title = styled.h1`
   font-size: 1.5em;
 `
 
-const Intro = styled.p`
-  font-size: large;
-`
-
 class App extends Component {
   render() {
     return (
-      <AppStyle>
-        <Header>
-          <Logo src={logo} alt="logo" />
-          <Title>Welcome to React</Title>
-        </Header>
-        <Intro>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </Intro>
-      </AppStyle>
+      <Router>
+        <AppStyle>
+          <Nav>
+            <ul>
+              <li>
+                <Link to="/boards">Boards</Link>
+              </li>
+              <li>
+                <Link to="/quests">Quests</Link>
+              </li>
+            </ul>
+          </Nav>
+          <Header>
+            <Logo src={logo} alt="logo" />
+            <Title>Welcome to React</Title>
+          </Header>
+          <Switch>
+            <Redirect from="/" exact to="/boards" />
+            <Route path="/boards">
+              <h1>Boards</h1>
+            </Route>
+            <Route path="/quests">
+              <h1>Quests</h1>
+            </Route>
+          </Switch>
+        </AppStyle>
+      </Router>
     )
   }
 }
