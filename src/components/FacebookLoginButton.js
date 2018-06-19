11b52652f66eb11ac32db5eda4eadeae703c62d2
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-06-19 23:52:37 +0700

    :sparkles: Save user profile url into the database

--- src/components/FacebookLoginButton.js
+++ src/components/FacebookLoginButton.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import firebase from 'firebase/app'
 import styled from 'styled-components'
+import axios from 'axios'
+
+import { setUser } from '../data'
 
 const Button = styled.button`
   border-radius: 4px;
@@ -27,16 +30,24 @@ const Span = styled.span`
 class FacebookLoginButton extends Component {
   login = async () => {
     const provider = new firebase.auth.FacebookAuthProvider()
+    provider.addScope('user_link')
 
     try {
       const result = await firebase.auth().signInWithRedirect(provider)
 
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      //const token = result.credential.accessToken
+      const token = result.credential.accessToken
       // The signed-in user info.
       const user = result.user
-
-      console.log(user)
+      const facebookUid = user.providerData[0].uid
+      const profileUrl = await axios
+        .get(
+          `https://graph.facebook.com/${facebookUid}?fields=link&access_token=${token}`
+        )
+        .then(res => res.data.link)
+      setUser(user.uid, {
+        url: profileUrl
+      })
     } catch (error) {
       // Handle Error here.
       const errorCode = error.code
