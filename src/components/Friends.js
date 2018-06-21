9eb6bc8becc21e7e8e318dc7f70b82c3185685b1
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-06-21 20:32:38 +0700

    :children_crossing::recycle: Get friends from store

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,39 +1,18 @@
-import React, { Component } from 'react'
-import { getFriends } from '../data'
+import React from 'react'
 
 import FriendListItem from './FriendListItems'
+import { StoreConsumer } from '../store'
 
-class Friends extends Component {
-  state = {
-    friends: null
-  }
-
-  componentDidMount() {
-    const { user } = this.props
-
-    if (user) this.fetchFriends(user.uid)
-  }
-
-  fetchFriends = async uid => {
-    const friends = await getFriends(uid)
-    this.setState({
-      friends
-    })
-  }
-
-  render() {
-    const { friends } = this.state
-
-    return (
+const Friends = () => (
+  <StoreConsumer>
+    {({ friends }) => (
       <div className="container">
         <h1>Friends</h1>
-        {!friends ||
-          friends.map((friend, index) => (
-            <FriendListItem friend={friend} key={index} />
-          ))}
+        {friends.map(friend => (
+          <FriendListItem friend={friend} key={friend.uid} />
+        ))}
       </div>
-    )
-  }
-}
-
+    )}
+  </StoreConsumer>
+)
 export default Friends
