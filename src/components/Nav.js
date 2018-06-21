d32b8bf69131db4b83a66232262e1152aa5d14c5
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-06-21 18:13:49 +0700

    :fire: Remove console log

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,41 +5,39 @@ import { Avatar } from './Avatar'
 
 export const Nav = ({ onLogout }) => (
   <StoreConsumer>
-    {({ user }) =>
-      console.log(user) || (
-        <ul className="mx-2 my-1 nav nav-pills">
-          <li className="nav-item">
-            <NavLink className="nav-link px-2 py-1" to="/posts">
-              Board
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link px-2 py-1" to="/quests">
-              Quests
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link px-2 py-1" to="/friends">
-              Friends
-            </NavLink>
-          </li>
+    {({ user }) => (
+      <ul className="mx-2 my-1 nav nav-pills">
+        <li className="nav-item">
+          <NavLink className="nav-link px-2 py-1" to="/posts">
+            Board
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link px-2 py-1" to="/quests">
+            Quests
+          </NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link px-2 py-1" to="/friends">
+            Friends
+          </NavLink>
+        </li>
 
-          {user && (
-            <React.Fragment>
-              <Avatar
-                className="ml-auto"
-                height={32}
-                src={`${user.photoURL}?height=32`}
-              />
-              <li>
-                <button className="btn btn-link px-2 py-1" onClick={onLogout}>
-                  Logout
-                </button>
-              </li>
-            </React.Fragment>
-          )}
-        </ul>
-      )
-    }
+        {user && (
+          <React.Fragment>
+            <Avatar
+              className="ml-auto"
+              height={32}
+              src={`${user.photoURL}?height=32`}
+            />
+            <li>
+              <button className="btn btn-link px-2 py-1" onClick={onLogout}>
+                Logout
+              </button>
+            </li>
+          </React.Fragment>
+        )}
+      </ul>
+    )}
   </StoreConsumer>
 )
