524ec91213034e380a522b40f224a5065e30a86f
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-06-22 19:02:18 +0700

    :bug: Change person who just get added's OTP

--- src/logic/friends.js
+++ src/logic/friends.js
@@ -2,11 +2,11 @@ import { queryUser, setFriend, deleteOtp } from '../data'
 import { generateAndSaveOtpToDb } from './login'
 
 export const handleAddFriendWithOtp = async (otp, myUser) => {
-  const user = await queryUser(['otp', '==', otp])
-  if (user) {
-    setFriend(myUser.uid, user)
-    deleteOtp(myUser.otp)
-    generateAndSaveOtpToDb(myUser.uid)
+  const friend = await queryUser(['otp', '==', otp])
+  if (friend) {
+    setFriend(myUser.uid, friend)
+    deleteOtp(friend.otp)
+    generateAndSaveOtpToDb(friend.uid)
     console.log('friend added')
   } else {
     console.log('no user with this otp')
