be8367c371a4034e1cf4ef59a2c91b44de5e8c4c
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2018-06-24 01:11:21 +0700

    :lipstick: render create thread button only if on thread list

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,13 +10,19 @@ class Board extends Component {
     return (
       <div className="container p-3 text-left">
         <h1 className="text-center">Boards</h1>
-        <div className="d-flex align-items-center py-3">
-          <div className="ml-auto">
-            <Link to="/posts/create" className="btn btn-primary">
-              Create Thread
-            </Link>
-          </div>
-        </div>
+        <Route
+          exact
+          path="/posts"
+          render={() => (
+            <div className="d-flex align-items-center py-3">
+              <div className="ml-auto">
+                <Link to="/posts/create" className="btn btn-primary">
+                  Create Thread
+                </Link>
+              </div>
+            </div>
+          )}
+        />
 
         <Switch>
           <Route exact path="/posts" component={PostList} />
