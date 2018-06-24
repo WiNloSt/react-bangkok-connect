30c5fb8385d7b6b783ccfb040c98c688d108f50d
Pakapon Kaewmark <sleep191@gmail.com>
2018-06-24 17:10:59 +0700

    :pencil2: Fix typo at Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -178,7 +178,7 @@ export const Nav = ({ onLogout }) => (
                 className="nav-link px-3 py-2 mx-2"
                 to="/liveleaderboard"
               >
-                LeaderBoard
+                Leaderboard
               </NavLink>
             </li>
 
